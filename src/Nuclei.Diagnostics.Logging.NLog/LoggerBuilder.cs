4682fa0329f399ab1711133912cfe9076b70b8ba
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-08-27 22:00:15 +1200

    Adding ability to create a logger from information stored in the configuration file
    references #3

--- src/Nuclei.Diagnostics.Logging.NLog/LoggerBuilder.cs
+++ src/Nuclei.Diagnostics.Logging.NLog/LoggerBuilder.cs
@@ -18,8 +18,92 @@ namespace Nuclei.Diagnostics.Logging.NLog
     /// </summary>
     public static class LoggerBuilder
     {
-        private static Target BuildFileTarget(string filePath)
+        private static LogFactory BuildLogFactory(string name, LogLevel minimumLevel, Target target)
         {
+            var config = new LoggingConfiguration();
+            {
+                config.AddTarget(name, target);
+
+                // define only one logging rule. We log everything (*) to the this rule starting
+                // at log level TRACE and everything goes to the only target
+                config.LoggingRules.Add(new LoggingRule("*", minimumLevel, target));
+            }
+
+            var result = new LogFactory(config);
+            {
+                result.GlobalThreshold = LogLevel.Trace;
+                result.ThrowExceptions = true;
+            }
+
+            result.ResumeLogging();
+            return result;
+        }
+
+        /// <summary>
+        /// Builds an <see cref="ILogger"/> object that logs information to the event log.
+        /// </summary>
+        /// <param name="loggerName">The name of the logger.</param>
+        /// <param name="eventLogSource">The event log source name under which the current application is registered.</param>
+        /// <param name="applicationName">The name of the application that has instantiated the logger.</param>
+        /// <param name="applicationVersion">The version of the application that has instantiated the logger.</param>
+        /// <returns>
+        /// The newly created logger that logs information to the event log.
+        /// </returns>
+        public static ILogger ForEventLog(
+            string loggerName,
+            string eventLogSource,
+            string applicationName = null,
+            Version applicationVersion = null)
+        {
+            var eventLogTarget = new EventLogTarget
+            {
+                // Only write the message. The message should contain all the important
+                // information anyway.
+                Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}",
+
+                // The source which has been registered to write to the event log.
+                Source = eventLogSource,
+
+                // Always write to the application log for now.
+                Log = "Application",
+
+                // Define how we move the event id to the logger.
+                EventId = string.Format(CultureInfo.InvariantCulture, "${{event-context:item={0}}}", AdditionalLogMessageProperties.EventId),
+
+                // Define how we move the event category to the logger.
+                Category = string.Format(CultureInfo.InvariantCulture, "${{event-context:item={0}}}", AdditionalLogMessageProperties.EventCategory),
+            };
+
+            var factory = BuildLogFactory(loggerName, LogLevel.Warn, eventLogTarget);
+            var logger = factory.GetLogger(loggerName);
+            return new NLogLogger(logger, applicationName, applicationVersion);
+        }
+
+        /// <summary>
+        /// Builds an <see cref="ILogger"/> object that logs information to a given file.
+        /// </summary>
+        /// <param name="loggerName">The name of the logger.</param>
+        /// <param name="filePath">The file path to which the information gets logged.</param>
+        /// <param name="applicationName">The name of the application that has instantiated the logger.</param>
+        /// <param name="applicationVersion">The version of the application that has instantiated the logger.</param>
+        /// <returns>
+        /// The newly created logger that logs information to a given file.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown if <paramref name="loggerName"/> is <see langword="null" />.
+        /// </exception>
+        public static ILogger ForFile(string loggerName, string filePath, string applicationName = null, Version applicationVersion = null)
+        {
+            if (loggerName == null)
+            {
+                throw new ArgumentNullException("loggerName");
+            }
+
+            if (string.IsNullOrWhiteSpace(loggerName))
+            {
+                throw new ArgumentException();
+            }
+
             var fileTarget = new FileTarget
             {
                 // Only write the message. The message should contain all the important
@@ -46,104 +130,29 @@ namespace Nuclei.Diagnostics.Logging.NLog
                 ConcurrentWrites = false,
             };
 
-            return fileTarget;
-        }
-
-        private static Target BuildEventLogTarget(string eventLogSource)
-        {
-            var eventLogTarget = new EventLogTarget
-            {
-                // Only write the message. The message should contain all the important
-                // information anyway.
-                Layout = "${longdate}|${level:uppercase=true}|${logger}|${message}",
-
-                // The source which has been registered to write to the event log.
-                Source = eventLogSource,
-
-                // Always write to the application log for now.
-                Log = "Application",
-
-                // Define how we move the event id to the logger.
-                EventId = string.Format(CultureInfo.InvariantCulture, "${{event-context:item={0}}}", AdditionalLogMessageProperties.EventId),
-
-                // Define how we move the event category to the logger.
-                Category = string.Format(CultureInfo.InvariantCulture, "${{event-context:item={0}}}", AdditionalLogMessageProperties.EventCategory),
-            };
-
-            return eventLogTarget;
-        }
-
-        private static LogFactory BuildLogFactory(string name, LogLevel minimumLevel, Target target)
-        {
-            var config = new LoggingConfiguration();
-            {
-                config.AddTarget(name, target);
-
-                // define only one logging rule. We log everything (*) to the this rule starting
-                // at log level TRACE and everything goes to the only target
-                config.LoggingRules.Add(new LoggingRule("*", minimumLevel, target));
-            }
-
-            var result = new LogFactory(config);
-            {
-                result.GlobalThreshold = LogLevel.Trace;
-                result.ThrowExceptions = true;
-            }
-
-            result.ResumeLogging();
-            return result;
+            var factory = BuildLogFactory(loggerName, LogLevel.Trace, fileTarget);
+            var logger = factory.GetLogger(loggerName);
+            return new NLogLogger(logger, applicationName, applicationVersion);
         }
 
         /// <summary>
-        /// Builds an <see cref="ILogger"/> object that logs information to a given file.
+        /// Builds an <see cref="ILogger"/> object that logs information to the log target as specified by the NLog configuration
+        /// of the same name.
         /// </summary>
-        /// <param name="filePath">The file path to which the information gets logged.</param>
-        /// <param name="template">The log template that handles the translation from the <see cref="ILogMessage"/> to the log text.</param>
+        /// <param name="loggerName">The name of the logger.</param>
         /// <param name="applicationName">The name of the application that has instantiated the logger.</param>
         /// <param name="applicationVersion">The version of the application that has instantiated the logger.</param>
         /// <returns>
-        /// The newly created logger that logs information to a given file.
+        /// The newly created logger that logs information to the log target as specified by the NLog configuration
+        /// of the same name.
         /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown if <paramref name="template"/> is <see langword="null" />.
-        /// </exception>
-        public static ILogger ForFile(string filePath, ILogTemplate template, string applicationName = null, Version applicationVersion = null)
-        {
-            if (template == null)
-            {
-                throw new ArgumentNullException("template");
-            }
-
-            var factory = BuildLogFactory(template.Name, LogLevel.Trace, BuildFileTarget(filePath));
-            return new NLogLogger(factory, template, applicationName, applicationVersion);
-        }
-
-        /// <summary>
-        /// Builds an <see cref="ILogger"/> object that logs information to the event log.
-        /// </summary>
-        /// <param name="eventLogSource">The event log source name under which the current application is registered.</param>
-        /// <param name="template">The log template that handles the translation from the <see cref="ILogMessage"/> to the log text.</param>
-        /// <param name="applicationName">The name of the application that has instantiated the logger.</param>
-        /// <param name="applicationVersion">The version of the application that has instantiated the logger.</param>
-        /// <returns>
-        /// The newly created logger that logs information to the event log.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Thrown if <paramref name="template"/> is <see langword="null" />.
-        /// </exception>
-        public static ILogger ForEventLog(
-            string eventLogSource,
-            ILogTemplate template,
+        public static ILogger FromConfiguration(
+            string loggerName,
             string applicationName = null,
             Version applicationVersion = null)
         {
-            if (template == null)
-            {
-                throw new ArgumentNullException("template");
-            }
-
-            var factory = BuildLogFactory(template.Name, LogLevel.Warn, BuildEventLogTarget(eventLogSource));
-            return new NLogLogger(factory, template, applicationName, applicationVersion);
+            var logger = LogManager.GetLogger(loggerName);
+            return new NLogLogger(logger, applicationName, applicationVersion);
         }
     }
 }
